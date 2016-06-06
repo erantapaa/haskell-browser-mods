9f22851164894ff059c5070c84210534185c8a15
Erik Rantapaa <erantapaa@gmail.com>
2016-06-05 20:00:24 -0500

    Add HN mods

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Inspired by and icons borrowed from [hackage-fu](https://github.com/statusfailed/hackage-fu).
+
+A simple Chrome browser extension to modify pages at http://hdiff.luite.com
+
+Currently it just performs one modification:
+
+- the urls to Hackage packages are turned into a real links
+
---
+++ chrome/hn-injected.js
@@ -0,0 +1,32 @@
+
+function add_hdiff(n, oldhtml) {
+  var href = this.href;
+  $(this).attr("seen", 1);
+  var m = href.match("haskell.org.package/(.*?)-([0-9][^/]*)$");
+  if (m) {
+    var url = "http://hdiff.luite.com/cgit/" + m[1] + "/diff/?tag=" + m[2];
+    return ' <a href="' + url + '" target="_blank">(hdiff)</a> ';
+  } else {
+    return "";
+  }
+}
+
+function add_hdiff_links() {
+  $("table.table a[seen!=1][href^='https://hackage.haskell.org/package/']").after(add_hdiff);
+}
+
+function update_items() {
+  getNewItems();
+  add_hdiff_links();
+}
+
+(function() {
+  var old_setInterval = window.setInterval;
+  window.setInterval = function() { return 10 };
+  var last = old_setInterval(";", 1000);
+  for (var i = 0; i <= last; i++) { clearInterval(i); }
+  old_setInterval(update_items, 20*1000);
+  old_setInterval(refreshDates, 1000);
+  add_hdiff_links();
+})();
+
