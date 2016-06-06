5d902c83563405371a9039d9479cdc8a4fb1e71a
Erik Rantapaa <erantapaa@gmail.com>
2016-06-06 09:16:31 -0500

    Updates to hackage.js

--- chrome/hackage.js
+++ chrome/hackage.js
@@ -64,7 +64,7 @@ function parse_url(url) {
 
   if (parts.length < 3) {
     loc.area = "contents"
-  } else if (parts.length >= 3) {
+  } else if (parts.length >= 4) {
       var x = parts[3]
       if (x == "src") {
         loc.area = "docs-src"
@@ -121,55 +121,100 @@ function all_versions() {
   });
 }
 
-function add_reverse_depends() {
-  // add link to reverse_dependencies
-  // http://packdeps.haskellers.com/reverse/4Blocks
+function change_source_repo_link() {
+  // on the contents page, if the Source Repository
+  // is a git://github.com/... link, change it to http://github.com/...
 
-  pkgver_from_html()
+  console.log("in change_source_repo_link")
 
-  var url = window.location.href
-  var m = parse_full_url(url)
-  if (!m || !m.version) {
-    m = pkgver_from_html()
+  var isSourceRepo = function(i,e) {
+    return (this.textContent == "Source repository")
   }
-  console.log("m:", m)
-  if (m) {
-    var revurl = "http://packdeps.haskellers.com/reverse/" + m.package
-    var hdiff;
-    if (m.version) {
-      hdiff = "http://hdiff.luite.com/cgit/"+m.package+"/diff/?tag="+m.version
-    } else {
-      hdiff = "http://hdiff.luite.com/cgit/"+m.package+"/diff"
-    }
-    var docs;
-    if (m.version) {
-      docs = "/package/" + m.package + "-" + m.version + "/docs/doc-index-All.html"
-    } else {
-      docs = "/package/" + m.package + "/docs/doc-index-All.html"
-    }
 
-    var rev_anchor = '<a href="' + revurl + '">(rev-depends)</a>'
-    var hdiff_anchor = '<a href="' + hdiff + '">(hdiff)</a>'
-    var doc_anchor = '<a href="' + docs + '">(docs)</a>'
+  $("table.properties th")
+    .filter(isSourceRepo)
+    .first()
+    .siblings("td")
+    .first()
+    .each(function(i,e) {
+      console.log("found this td:", this)
+    })
+    // .find("a") .each(function(i,e) { console.log("--- and this a:", this) })
+    .find("a[href*='git://github.com/']")
+    .each(function(i,e) {
+      console.log("found source repo href:", this.href)
+      var href = this.href
+      if (href.match(/^git:/)) {
+        this.href = href.replace(/^git:/, "https:")
+        console.log("--- replaced git:// with https://")
+        // insert a new text node
+        var txt = document.createTextNode(this.href)
 
-    console.log("rev_anchor:", rev_anchor)
-    console.log("hdiff_anchor:", hdiff_anchor)
+        // delete all children of the A tag
+        while (this.firstChild) { this.removeChild(this.firstChild) }
+        this.appendChild(txt)
+      }
+    });
+}
 
-    var inner = rev_anchor + '&nbsp;' + hdiff_anchor + '&nbsp;' + doc_anchor
-    $("table.properties tbody").append(
-     '<tr><th>Additional Info</th><td>' + inner + '</td></tr>'
-    )
+function contents_page(m) {
+  // perform actions on a package's contents page
+  //
+  // m.package, m.version contain the package name and version
+
+  var revurl = "http://packdeps.haskellers.com/reverse/" + m.package
+  var hdiff;
+  if (m.version) {
+    hdiff = "http://hdiff.luite.com/cgit/"+m.package+"/diff/?tag="+m.version
+  } else {
+    hdiff = "http://hdiff.luite.com/cgit/"+m.package+"/diff"
   }
+  var docs;
+  if (m.version) {
+    docs = "/package/" + m.package + "-" + m.version + "/docs/doc-index-All.html"
+  } else {
+    docs = "/package/" + m.package + "/docs/doc-index-All.html"
+  }
+
+  var rev_anchor = '<a href="' + revurl + '">(rev-depends)</a>'
+  var hdiff_anchor = '<a href="' + hdiff + '">(hdiff)</a>'
+  var doc_anchor = '<a href="' + docs + '">(docs)</a>'
+
+  console.log("rev_anchor:", rev_anchor)
+  console.log("hdiff_anchor:", hdiff_anchor)
+
+  var inner = rev_anchor + '&nbsp;' + hdiff_anchor + '&nbsp;' + doc_anchor
+  $("table.properties tbody").append(
+   '<tr><th>Additional Info</th><td>' + inner + '</td></tr>'
+  )
+
+  change_source_repo_link()
 }
 
 function main() {
-  add_reverse_depends()
+  var loc = parse_full_url(window.location.href)
+  console.log(loc)
+  if (!loc) return;
+
+  if (loc.area == "contents") {
+    if (!loc.version) {
+      loc2 = pkgver_from_html()
+    } else {
+      loc2 = loc
+    }
+    contents_page(loc2)
+  }
 }
 
-module.exports = {
-  url_path: url_path,
-  parse_url: parse_url,
-  parse_full_url: parse_full_url,
-  parse_modname: parse_modname
+if (typeof document === 'undefined') {
+  // running under node
+  module.exports = {
+    url_path: url_path,
+    parse_url: parse_url,
+    parse_full_url: parse_full_url,
+    parse_modname: parse_modname
+  }
+} else {
+  main()
 }
 
