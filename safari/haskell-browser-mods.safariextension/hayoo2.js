f6fe2e835d1de5da04e18dbf5be7dd705fcc2be0
Erik Rantapaa <erantapaa@gmail.com>
2016-07-01 03:51:40 -0500

    Hayoo mods for Safari

---
+++ safari/haskell-browser-mods.safariextension/hayoo2.js
@@ -0,0 +1,169 @@
+
+
+// return a shortened version of div
+function shorten(target) {
+  // iterate over children
+  var child = target.firstChild
+  if (!child) return null;
+  if (child.tagName == "PRE") {
+    return null
+  } else {
+    // assume a block element - iterate over children of child
+    var e = child.firstChild
+    var len = 0
+    var count = 0
+    var e = child.firstChild
+    var maxlength = 100
+    var last_text = ""
+    for (; e; e = e.nextSibling) {
+      var newl = len + e.textContent.length
+      if (newl > maxlength) {
+        if (e.nodeType == 3) { // a text node
+          // find first space after
+          var text = e.textContent
+          var k = text.indexOf(' ', maxlength - len)
+          if (k >= 0) {
+            last_text = text.substr(0, k)
+          } else {
+            last_text = text
+          }
+        } else { // an element
+          count++
+        }
+        break
+      }
+      len = newl
+      count++
+    }
+    if (e) {
+      // need to add More..., copy first n nodes
+      var frag = document.createElement(child.tagName)
+      var j = 0;
+      for (var ee = child.firstChild; ee && (j < count); ee = ee.nextSibling, j++) {
+        frag.appendChild( ee.cloneNode(true) )
+      }
+      frag.appendChild( document.createTextNode(last_text + " ...") )
+      return frag
+    } else {
+      // no need to add ellipses
+      if (child.nextSibling) {
+        var frag = child.cloneNode(true)
+        frag.appendChild( document.createTextNode(" ...") )
+        return frag
+      } else {
+        return null // no need to provide short version
+      }
+    }
+  }
+}
+
+function isEllipsisActive(e) {
+   var tolerance = 2; // In px. Depends on the font you are using
+   return e.offsetWidth + tolerance < e.scrollWidth;
+}
+
+function handle_more_less(e) {
+  $(this.parentNode.parentNode.nextSibling).children().toggle()
+}
+
+function handle_expand_collapse(me) {
+  var div = me.parentNode.nextSibling
+  $(div).children().toggle()
+
+  if (me.className == "collapser") {
+    me.className = "expander"
+  } else {
+    me.className = "collapser"
+  }
+  return false;
+}
+
+function insert_more_control(target) {
+  // <div><p><a class="more-control">More/Less</a></div>
+  $(".more").each(function () {
+    var div = document.createElement("div")
+    div.innerHTML = '<p><a class="more-control">More/Less</a>'
+    this.parentNode.insertBefore(div, this)
+  })
+  $(".more-control").each(function() {
+    this.onclick = handle_more_less
+  })
+
+}
+
+
+// On entry we have this structure:
+//
+//   <div class="description more">
+//     <div class="preview">
+//     <div class="content">
+//
+//  - or -
+//
+//   <div class="description more">...text...</div>
+//
+// We want this structure:
+//   <div class="more-div">        -- for the expand/collapse control
+//   <div class="description more">
+//     first child: preview element
+//     <div class="content">...</div>
+
+
+function fix_descriptions() {
+  // var content_divs = document.getElementsByClassName("more").length
+  // console.log("=== more div count:", content_divs)
+
+  $(".more").each(function() {
+    var found = 0
+    $(this).find("div.preview").each(function() {
+      $(this).remove()
+      found = 1
+    })
+
+    if (found) {
+
+      // fix up the .content div
+      $(this).find(".content").each(function() {
+        var text = $(this).text()
+        if (text.substr(text.length-4) == "less") {
+          text = text.substr(0, text.length-4)
+        }
+        $(this).html(text)
+        $(this).find('*').show()
+        $(this).show()
+      })
+
+    } else {
+      // create the .content div
+      var text = $(this).text()
+      var div = document.createElement("div")
+      div.className = "content"
+      $(div).html(text)
+      $(this).empty().append(div)
+    }
+  })
+
+  // shorten each .content div if necessary
+
+  $(document).find(".content").each(function() {
+    if (this.firstChild) {
+      var short = shorten(this)
+      if (short) {
+
+        // create the new preview element
+        this.parentNode.insertBefore(short, this.parentNode.firstChild)
+
+        // insert control div just before this one
+        var div = document.createElement("div")
+        div.innerHTML = '<p class="expander" onclick="handle_expand_collapse(this)">&nbsp;'
+        div.className = "more-div"
+        this.parentNode.parentNode.insertBefore(div, this.parentNode)
+        $(this).hide()
+      }
+    }
+  })
+}
+
+console.log("== in hayoo2.js")
+fix_descriptions()
+
--- safari/haskell-browser-mods.safariextension/main.js
+++ safari/haskell-browser-mods.safariextension/main.js
@@ -14,26 +14,34 @@ function insert_css(resource) {
 }
 
 function init() {
-  var where = window.location.href
+  var where = new URL(window.location.href)
+  var host = where.hostname
   console.log("where:", where)
-  if (where.match("hdiff\.luite\.com")) {
+  if (host == "hdiff.luite.com") {
     insert_script("hdiff.js")
-  } else if (where.match("haskellnews.org")) {
+  } else if (host == "haskellnews.org") {
     insert_script("haskellnews.js")
-  } else if (where.match('hackage.haskell.org/package')) {
-    console.log("I'm in hackage!")
-    insert_script("jquery-1.12.4.min.js")
-    insert_css("ocean-patch.css")
-    // check if on index page
-    if (where.match("docs/doc-index(-All)?.html")) {
-      console.log("=== in the doc-index area")
-      insert_css("awesomplete.css")
-      insert_script("haddock-index.js")
-    } else {
-      console.log("=== not in the doc-index area")
+  } else if (host == "hackage.haskell.org") {
+    if (where.pathname.match("^/package/")) {
+      // where.match('hackage.haskell.org/package')) {
+      console.log("I'm in hackage!")
+      insert_script("jquery-1.12.4.min.js")
+      insert_css("ocean-patch.css")
+      // check if on index page
+      if (where.pathname.match("docs/doc-index(-All)?.html")) {
+        console.log("=== in the doc-index area")
+        insert_css("awesomplete.css")
+        insert_script("haddock-index.js")
+      } else {
+        console.log("=== not in the doc-index area")
+      }
+      insert_script("mousetrap.min.js")
+      insert_script("hackage.js")
     }
-    insert_script("mousetrap.min.js")
-    insert_script("hackage.js")
+  } else if (host == "hayoo.fh-wedel.de") {
+    insert_css("hayoo.css")
+    insert_script("hayoo.js")
+    insert_script("hayoo2.js")
   } else {
     console.log("=== where am I???", where)
   }
